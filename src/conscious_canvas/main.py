cbf053144b5452d446b2b9f490876135a8562999
Aleks <aykamko@gmail.com>
2023-07-15 09:45:25 -0700

    saving audio from HTML to disk, nice

--- src/conscious_canvas/main.py
+++ src/conscious_canvas/main.py
@@ -1,7 +1,9 @@
+from typing import Annotated
 import logging
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, UploadFile
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import shutil
 
 from .image_util import pil_image_from_b64, pil_image_to_b64
 from .a1111 import generate_a1111_controlnet
@@ -30,6 +32,14 @@ async def generate(payload: GeneratePayload):
         "message": f"scribble_byte_len: {scribble_byte_len}",
         "img_b64": b64_converted,
     }
+
+
+@app.post("/whisper")
+async def whisper(audio_file: Annotated[UploadFile, Form()]):
+    # import ipdb; ipdb.set_trace()
+    with open("recording.wav", "wb") as out_file:
+        shutil.copyfileobj(audio_file.file, out_file)
+    return {"message": "whisper"}
 
 
 app.mount("/", StaticFiles(directory="web_static", html=True), name="web_static")
