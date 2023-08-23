39a6fb6be112cf25eba44bc92ac228b4e607d123
Aleks <aykamko@gmail.com>
2023-08-22 23:28:10 -0700

    wip: nsfw filtering

---
+++ src/conscious_canvas/nsfw.py
@@ -0,0 +1,16 @@
+from transformers import CLIPFeatureExtractor
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+from PIL import Image
+
+
+class NSFWChecker:
+    def __init__(self):
+        self.feature_extractor = CLIPFeatureExtractor.from_pretrained("openai/clip-vit-base-patch32")
+        self.checker = StableDiffusionSafetyChecker.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", subfolder="safety_checker"
+        )
+
+    def check_image(self, image: Image.Image) -> bool:
+        safety_checker_input = self.feature_extractor(images=image, return_tensors="pt")
+        _, has_nsfw_concept = self.safety_checker(images=image, clip_input=safety_checker_input.pixel_values)
+        return has_nsfw_concept[0]
